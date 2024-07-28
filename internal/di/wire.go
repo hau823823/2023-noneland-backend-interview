4d132eeb9d271c240f836b7153f2073b34fca234
hau <hau823823@gmail.com>
2024-07-29 03:07:06 +0800

    feat:Attempt to refactor the framework using Wire

--- internal/di/wire.go
+++ internal/di/wire.go
@@ -4,37 +4,65 @@
 package di
 
 import (
-	"github.com/google/wire"
-	"gorm.io/gorm"
 	"noneland/backend/interview/configs"
+	"noneland/backend/interview/internal/api"
 	"noneland/backend/interview/internal/db"
-	"noneland/backend/interview/internal/entity"
-	repo "noneland/backend/interview/internal/repo/gorm"
+	"noneland/backend/interview/internal/pkg"
 	"sync"
+
+	"github.com/google/wire"
 )
 
-var cg *configs.Config
-var configOnce sync.Once
+var (
+	configOnce sync.Once
+	cfg        *configs.Config
 
+	dbOnce   sync.Once
+	database *db.MySQLClient
+)
+
+// NewConfig 確保配置僅初始化一次
 func NewConfig() *configs.Config {
 	configOnce.Do(func() {
-		cg = configs.NewConfig()
+		cfg = configs.NewConfig()
 	})
-
-	return cg
+	return cfg
 }
 
-var database *gorm.DB
-var dbOnce sync.Once
-
-func NewDB() *gorm.DB {
+// NewDBClient 確保數據庫連接僅初始化一次
+func NewDBClient(cfg *configs.Config) (db.DBClient, error) {
+	var err error
 	dbOnce.Do(func() {
-		database = db.NewDb()
+		database, err = db.NewMySQLClient(cfg.DSN)
 	})
-
-	return database
+	return database, err
 }
 
-func NewRepo() (entity.Repository, error) {
-	panic(wire.Build(repo.NewRepository, NewDB, NewConfig))
+// NewExAPIClient 構造真實 API 客戶端
+func NewExAPIClient() pkg.APIClient {
+	return pkg.NewExAPIClient()
 }
+
+// NewAPI 構造真實的 API 實例
+func NewAPI() (*api.API, error) {
+	wire.Build(api.NewAPI, NewExAPIClient, pkg.NewCache, NewDBClient, NewConfig)
+	return &api.API{}, nil
+}
+
+// NewMockDBClient 構造 Mock DB 客戶端
+/**
+func NewMockDBClient() db.DBClient {
+	return db.NewMockDBClient()
+}
+
+// NewMockAPIClient 構造 Mock API 客戶端
+func NewMockAPIClient() pkg.APIClient {
+	return pkg.NewMockAPIClient()
+}
+
+// NewMockAPI 構造 Mock API 實例
+func NewMockAPI() (*api.API, error) {
+	wire.Build(api.NewAPI, NewMockAPIClient, pkg.NewCache, NewMockDBClient, NewConfig)
+	return &api.API{}, nil
+}
+*/
